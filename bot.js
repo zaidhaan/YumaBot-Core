fbd22c247b114bc36602214237a96c79c27125cb
Zaidhaan Hussain <zaidhanhussain@gmail.com>
2016-12-08 16:06:09 +0500

    more info in bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,7 @@
+/*
+This is an example of how you could run the bot, the variable client van also be replaced with require("YumaBot-Core");
+*/
+
 var client = require("./client.js");
 var config = require("./config.json");
 var bot = new client(config);

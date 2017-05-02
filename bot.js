614311f1c3747dae12856efb6e5cd71e2e4062e5
Vex <zaidhanhussain@gmail.com>
2017-05-02 15:35:06 +0500

    spelling mistake :eyes:

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 /*
-This is an example of how you could run the bot, the variable client van also be replaced with require("YumaBot-Core");
+This is an example of how you could run the bot, the variable client can also be replaced with require("YumaBot-Core");
 */
 
 var client = require("./client.js");
